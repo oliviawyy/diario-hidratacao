8ec066c685147d7203abf8491d732071e64519a3
oliviawyy <olivia.c@aluno.senai.br>
2026-09-18 10:42:05 -0300

    refactor: restructure app components and remove unused files

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { COLORS } from './src/constants/colors';
+import { Header } from './src/components/Header';
+import { WaterProgress } from './src/components/WaterProgress';
+import { ActionButtons } from './src/components/ActionButtons';
+
+export default function App() {
+    const GOAL = 2000; // Meta diária em ml
+    const [consumed, setCosumed] =  useState(0);
+
+    // funcao para acumular a quantidade ingerida
+    const handleAddWater = (amount) => {
+
+    };
+
+    //funcao para zerar o contador
+    const handleReset = () => {
+
+    };
+
+    return (
+      <SafeAreaProvider>
+        <SafeAreaView>
+    
+        </SafeAreaView>
+      </SafeAreaProvider>
+    )
+    
+  
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+
---
+++ src/constants/colors.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+    background: '#F0F9FF',
+    cardBg: '#FFFFFF',
+    primary: '#0284C7',
+    secondary: '#3BBDF8',
+    textMain: '#0C4A6E',
+    textMuted: '#64748B',
+    danger: '#EF4444'
+
+};
+
+export default COLORS
