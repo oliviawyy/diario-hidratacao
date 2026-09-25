85fc68df20acf4c2683942fac699b0cc01332b43
oliviawyy <olivia.c@aluno.senai.br>
2026-09-25 08:56:18 -0300

    tentando resolver o problema do button de adicionar, mas ja usei o pressable inves do Button

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -1,29 +1,64 @@
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import { COLORS } from '../constants/colors';
 
-export function ActionButtons() {
+export function ActionButtons({onAddWater}) {
     return (
         <View>
 
-        <Text>Adicionar Consumo</Text>
-        {/* <Button>+200 ml</Button>
-        <Button>+350 ml</Button>
-        <Button>+500 ml</Button>
-        <Button>🔁 Resetar Dia</Button> */}
+            <Text style={styles.text}>Adicionar Consumo</Text>
+
+            <View>
+                <Pressable style={styles.buttonAdd} onPress={() =>onAddWater(200)}>
+                    <Text style={styles.textoBotao}>+200 ml</Text>
+                </Pressable>
+
+                <Pressable style={styles.buttonAdd} onPress={() => onAddWater(350)}>
+                    <Text style={styles.textoBotao}>+350 ml</Text>
+                </Pressable>
+
+                <Pressable style={styles.buttonAdd} onPress={() => onAddWater(500)}>
+                    <Text style={styles.textoBotao}>+500 ml</Text>
+                </Pressable>
+            </View>
+
+            <Pressable style={styles.botaoReset} onPress={() => alert('Resetou o dia')}>
+                <Text style={styles.textoReset}>🔁 Resetar Dia</Text>
+            </Pressable>
 
         </View>
 
     );
 
-    
+
 }
 
 const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+    },
+
+
     text: {
         color: COLORS.textMain,
         fontSize: 16,
         textAlign: 'center',
     },
+
+    buttonAdd: {
+        padding: 10,
+        backgroundColor: COLORS.primary,
+        borderRadius: 10,
+        marginRight: 10,
+        width: 100,
+    },
+
+    botaoReset: {
+        padding: 10,
+        backgroundColor: COLORS.danger,
+        borderRadius: 10,
+        alignItems: 'center',
+        flex: 1,
+        width: 100,
+    },
 });
 
-export default ActionButtons
